d027cd259f1f3a12f99b2aaf599adf01f85f586c
glaza <33300759+glaza@users.noreply.github.com>
2021-09-24 18:30:12 -0400

    try to add value instead of pressing.

--- zoomfit.js
+++ zoomfit.js
@@ -3,37 +3,37 @@
 /**
  * Presses the zoom area
  */
-function zoom() 
-{
-    var inputSelector = "#zoomSelect input.jfk-textinput";
-    if ( $( inputSelector ).length > 0 ) 
-    {
-        console.log( "Menu loaded, Zoom Fitting..." );
-
-        $( inputSelector ).simulate( 'mousedown' );
-
-        setTimeout( fit, 100 );
-    }
-    else // retry 
-    {
-        setTimeout( zoom, 100 );
+function zoom() {
+    var inputSelector = $("#zoomSelect input.jfk-textinput");
+    if (inputSelector.length > 0) {
+        inputSelector.val("Fit");
+        //inputSelector.change();
+        // var e = jQuery.Event("keypress");
+        // e.which = 13; //choose the one you want
+        // e.keyCode = 13;
+        // inputSelector.trigger(e);
+        // inputSelector.simulate( keyEvent, {
+		// 	keyCode: 70
+		// });
+        // inputSelector.simulate( keyEvent, {
+		// 	keyCode: 13
+		// });
+    } else { // retry 
+        setTimeout(zoom, 100);
     }
 }
 
 /**
  * Selects the Fit zoom level
  */
-function fit()
-{
+function fit() { ".docs-gm .goog-menuitem .goog-menuitem-content"
     var fitSelector = "div.goog-menu[style*='visible']:first div:first";
-    if ( $( fitSelector ).length > 0 )
-    {
-        $( fitSelector ).simulate( 'mouseup' );
-    }
-    else // retry
-    {
-        setTimeout( fit, 100 );
+    if ($(fitSelector).length > 0) {
+        console.log("ZoomFitGoogleDocs: Pressing Fit button...");
+        $(fitSelector).simulate('mousedown');
+    } else { // retry
+        setTimeout(fit, 100);
     }
 }
 
-setTimeout( zoom, 100 );
+setTimeout(zoom, 100);
